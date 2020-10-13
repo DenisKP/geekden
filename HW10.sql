12671f8509ac810c37b30feafd48b5f8e17498b3
DenisKP <64198029+DenisKP@users.noreply.github.com>
2020-10-13 17:49:17 +0300

    Update HW10.sql

--- HW10.sql
+++ HW10.sql
@@ -1,258 +1,30 @@
--- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
---
--- Host: localhost    Database: VK
--- ------------------------------------------------------
--- Server version	8.0.21-0ubuntu0.20.04.4
+-- 1
+USE VK2;
+CREATE INDEX name ON users(name);
+CREATE INDEX birthday ON profiles(birthday);
+CREATE INDEX post ON posts(id);
+CREATE INDEX media_type ON media_types(name);
+CREATE INDEX communitie ON communities_users(id);
+CREATE INDEX groups ON communities(group_id);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `communities`
---
-
-DROP TABLE IF EXISTS `communities`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `communities` (
-  `group_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'string identifier',
-  `group_name` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'group name',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'string update time',
-  PRIMARY KEY (`group_id`),
-  UNIQUE KEY `group_name` (`group_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='groups table';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `communities_users`
---
-
-DROP TABLE IF EXISTS `communities_users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `communities_users` (
-  `communities_id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'group identifier',
-  `user_id` int unsigned NOT NULL COMMENT 'link on user',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  PRIMARY KEY (`communities_id`,`user_id`) COMMENT 'composite primary key'
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='table with connections between users and groups';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `friendship`
---
-
-DROP TABLE IF EXISTS `friendship`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `friendship` (
-  `user_id` int unsigned NOT NULL COMMENT 'link on friendship initilizer',
-  `friend_id` int unsigned NOT NULL COMMENT 'link on friendship reciever',
-  `status_id` int unsigned NOT NULL COMMENT 'link on friendship status',
-  `requested_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'friendship time request',
-  `confirmed_at` datetime DEFAULT NULL COMMENT 'friendship approval time',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'string update time'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='friendship';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `friendship_statuses`
---
-
-DROP TABLE IF EXISTS `friendship_statuses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `friendship_statuses` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'string identifier',
-  `name` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'status name',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'string update time',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='frienship statuses';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `like_types`
---
-
-DROP TABLE IF EXISTS `like_types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `like_types` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `like_type` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `like_type` (`like_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `likes`
---
-
-DROP TABLE IF EXISTS `likes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `likes` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int unsigned NOT NULL,
-  `like_id` int unsigned NOT NULL,
-  `type_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `media`
---
-
-DROP TABLE IF EXISTS `media`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `media` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'string identifier',
-  `user_id` int unsigned NOT NULL COMMENT 'link on user id who uploaded file',
-  `filepath` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'path to file',
-  `file_size` int NOT NULL COMMENT 'file size',
-  `metadata` json DEFAULT NULL,
-  `media_type_id` int unsigned NOT NULL COMMENT 'link on content mediatype',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'string update time',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='mediafiles';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `media_types`
---
-
-DROP TABLE IF EXISTS `media_types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `media_types` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'string identifier',
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'type name',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'string update time',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='table with mediafile types';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `messages`
---
-
-DROP TABLE IF EXISTS `messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `messages` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'string identifier',
-  `from_user_id` int unsigned NOT NULL COMMENT 'Link on message sender',
-  `to_user_id` int unsigned NOT NULL COMMENT 'Link on message recipient',
-  `body` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'message text',
-  `is_important` tinyint(1) DEFAULT NULL COMMENT 'sign of importance',
-  `is_delivered` tinyint(1) DEFAULT NULL COMMENT 'delivery sign',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'string update time',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='messages';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `posts`
---
-
-DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `posts` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `post_type` int unsigned NOT NULL,
-  `post_content` varchar(255) NOT NULL,
-  `post_media` varchar(255) NOT NULL,
-  `user_id` int unsigned NOT NULL,
-  `like` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `profiles`
---
-
-DROP TABLE IF EXISTS `profiles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `profiles` (
-  `user_id` int unsigned NOT NULL COMMENT 'User link',
-  `gender` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL COMMENT 'gender',
-  `birthday` date DEFAULT NULL COMMENT 'birthday date',
-  `photo_id` int unsigned DEFAULT NULL COMMENT 'Link on main profile photo',
-  `city` varchar(130) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s current city',
-  `country` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s current country',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'string creation time',
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'string update time',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Profiles';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `storehouses_products`
---
-
-DROP TABLE IF EXISTS `storehouses_products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `storehouses_products` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `storehouse_name` varchar(255) DEFAULT NULL COMMENT 'STOREHOUSE NAME',
-  `product_name` varchar(255) DEFAULT NULL COMMENT 'ANY PRODUCT NAME',
-  `value` int unsigned DEFAULT NULL COMMENT 'PRODUCT VALUE AT STOREHOUSE',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `birthday_at` date DEFAULT NULL,
-  `created_at` varchar(255) DEFAULT NULL,
-  `updated_at` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-09-08 12:07:53
+-- 2
+USE VK;
+SELECT DISTINCT 
+  communities.group_name AS group_name,
+  COUNT(communities_users.user_id) OVER() 
+    / (SELECT COUNT(*) FROM communities) AS avg_users_in_groups,
+  FIRST_VALUE(CONCAT_WS(" ", users.first_name, users.last_name)) OVER w_community_birthday_desc AS youngest,
+  FIRST_VALUE(CONCAT_WS(" ", users.first_name, users.last_name)) OVER w_community_birthday_asc AS oldest,
+  COUNT(communities_users.user_id) OVER w_community AS users_in_group,
+  (SELECT COUNT(*) FROM users) AS users_total,
+  COUNT(communities_users.user_id) OVER w_community / (SELECT COUNT(*) FROM users) *100 AS '%%'
+    FROM communities
+      LEFT JOIN communities_users 
+        ON communities_users.community_id = communities.id
+      LEFT JOIN users 
+        ON communities_users.user_id = users.id
+      LEFT JOIN profiles 
+        ON profiles.user_id = users.id
+      WINDOW w_community AS (PARTITION BY communities.id),
+             w_community_birthday_desc AS (PARTITION BY communities.id ORDER BY profiles.birthday DESC),
+             w_community_birthday_asc AS (PARTITION BY communities.id ORDER BY profiles.birthday; 
